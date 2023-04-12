a00a10673e5b3f293b3922d34a0816a87092f458
Adam Price <github@adam.pappnase.co.uk>
2023-04-12 11:32:45 +0200

    Refactor

--- Post-Install.sh
+++ Post-Install.sh
@@ -1,29 +1,45 @@
 #!/bin/bash
 
-bold=$(tput bold)
-normal=$(tput sgr0)
-
-cd ~/ || exit
-
-#Checking to see if the system is a Nobara Linux
-if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Nobara"* ]]; then
-    echo "You are using Nobara"
-    
+bashbar()
+{
+    (( $1 > 100 ))&&{ printf 'Enter an integer from 1-100 (percent)\n'; exit 1; }
+    shopt -s checkwinsize; (:;:)
+    ((width=COLUMNS-4)); ((progress=$1*width/100))
+    if (( $1 > 68 )); then color=2
+    elif (( $1 > 34 )); then color=3; fi
+    printf '\r|\e[4%dm%*s\e[m' "${color:-1}" "$progress"
+    printf '\e[%dG|%d%%' "$width" "$1"
+    (( $1 == 100 ))&& printf '\n'
+}
+yesno()
+{
+    # yesno returns success (0) for 'Y', 'y', or any character in '[:space:]' failure (1) otherwise
+    local message="${1:-"y/n ?"}"
+    declare -u answer
+    read -r -n 1 -p "${message} " answer
+    [[ ${answer:-Y} =~ Y ]] && return 0
+    return 1
+}
+update()
+{
     #Updating
     echo -e "\e[1;31m${bold}Updating System${normal}\e[0m"
     sudo dnf up -y && echo "System Updated"
-    
+}
+install_flatpak()
+{
     #Installing Flatpak
-    read -p "Do you want to install Flatpak? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Flatpak? (Y/n) " ; then
         sudo dnf install --assumeyes flatpak && echo "Flatpak Installed"
     else
         echo "Moving On"
     fi
-    
+}   
+
+install_theming()
+{
     #Installing Theming and Customizing Applications
-    read -p "Do you want to install Customization Programs? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Customization Programs? (Y/n) " ; then
         flatpak install ExtensionManager -y && echo "Extension Manager Installed" || echo "Extension Manager Couldn't Installed"
         flatpak install com.github.GradienceTeam.Gradience -y && echo "Gradience Installed" || echo "Gradience Couldn't Installed"
         flatpak install org.gtk.Gtk3theme.adw-gtk3 -y && echo "Adwaita GTK 3 Theme for flatpaks Installed" || echo "Adwaita GTK 3 Theme for flatpaks Couldn't Installed"
@@ -33,46 +49,52 @@ if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Nobara"* ]]; then
     else
         echo "Moving on"
     fi
-
+}
+install_libre_office()
+{
     #Installing LibreOffice
-    read -p "Do you want to install LibreOffice? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install LibreOffice? (Y/n) " ; then 
         sudo dnf install --assumeyes libreoffice && echo "Libreoffice Installed" || echo "Libreoffice Couldn't Installed"
         sudo dnf install --assumeyes libreoffice-langpack-tr && echo "Libreoffice TR Language Pack Installed" || echo "Libreoffice TR Language Couldn't Installed"
         wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/install-papirus-root.sh | sh && echo "Libreoffice Papirus Theme Installed" || echo "Libreoffice Papirus Theme Couldn't Installed"
     else
         echo "Moving on"
     fi
+}
+install_gaming()
+{
     #Installing Gaming Applications
-    read -p "Do you want to install Gaming Applications? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Gaming Applications? (Y/n) " ; then
         flatpak install io.github.Foldex.AdwSteamGtk -y && echo "Steam Adwaita Theme Installed" || echo "Steam Adwaita Theme Couldn't Installed"
         flatpak install com.heroicgameslauncher.hgl -y && echo "Heroic Games Launcher Installed" || echo "Heroic Games Launcher Couldn't Installed"
     else
         echo "Moving on"
     fi
-    
-    #Installing Photo Editing and Drawing Applications
-    read -p "Do you want to install Photo Editing and Drawing Applications? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+}
+install_photo_tools()
+{
+        #Installing Photo Editing and Drawing Applications
+    if yesno "Do you want to install Photo Editing and Drawing Applications? (Y/n) " ; then
         sudo dnf install --assumeyes gimp && echo "GIMP Installed" || echo "GIMP Couldn't Installed"
         sudo dnf install --assumeyes inkscape && echo "Inkscape Installed" || echo "Inkscape Couldn't Installed"
         sudo dnf install --assumeyes blender && echo "Blender Installed" || echo "Blender Couldn't Installed"
     else
         echo "Moving on"
     fi
-    
+}
+install_virtualisation()
+{
     #Installing Virtualization Applications
-    read -p "Do you want to install Virtualization Applications? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Virtualization Applications? (Y/n) " ; then
         sudo dnf install --assumeyes gnome-boxes && echo "Boxes Installed" || echo "Boxes Couldn't Installed"
         sudo dnf install --assumeyes VirtualBox && echo "VirtualBox Installed" || echo "VİrtualBox Couldn't Installed"
         sudo dnf install --assumeyes waydroid && echo "Waydroid Installed" || echo "Waydroid Couldn't Installed"
     else
         echo "Moving on"
     fi
-    
-    #Downloading and Exctracting Configs
+}
+download_configs()
+{    #Downloading and Exctracting Configs
     echo -e "\e[1;31m${bold}Configurations Syncing${normal}\e[0m"
     cd ~/ || exit
     echo -e "\e[1;31m${bold}Downloading Configuration Files${normal}\e[0m"
@@ -85,21 +107,29 @@ if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Nobara"* ]]; then
     tar xvf adw-gtk3-dark.tar.xz
     rm adw-gtk3.tar.xz
     rm adw-gtk3-dark.tar.xz
-    
+}
+copy_configs()
+{
+        
     #Copying Configs
     cd ~/Post-Install || exit
     cp -Rv config/{gtk-2.0,gtk-3.0,gtk-4.0,dconf,menus} ~/.config/
     cp -Rv {font,themes,icons} ~/.local/share
     cp -Rv chrome ~/.mozilla/firefox/*.default-release/ 
     
+}
+cleanup_precopied_files()
+{
     #Removing the already copied unnecessary files
     cd ~/ || exit
     rm -rf ~/Post-Install
     echo -e "\e[1;31m${bold}Configuration Files are synced${normal}\e[0m"
-    
+}
+install_wallpaper()
+{
+        
     #Installing Wallpapers
-    read -p "Do you want to install Wallpapers? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Wallpapers? (Y/n) " ; then
     echo -e "\e[1;31m${bold}Installing Wallpapers${normal}\e[0m"
         git clone https://github.com/saint-13/Linux_Dynamic_Wallpapers.git  
         cd Linux_Dynamic_Wallpapers || exit
@@ -114,7 +144,9 @@ if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Nobara"* ]]; then
     else
         echo -e "\e[1;31m${bold}Moving On${normal}\e[0m"
     fi
-     
+}
+apply_settings()
+{
     #Applying Some Settings
     echo -e "\e[1;31m${bold}Applying Some Settings${normal}\e[0m"
     fc-cache -r
@@ -125,21 +157,19 @@ if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Nobara"* ]]; then
     gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
     gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/Dynamic_Wallpapers/MaterialMountains/MaterialMountains-1.png
     echo -e "\e[1;31m${bold}Installation Finished!${normal}\e[0m"
-else 
-    echo "You are not using Nobara checking others"
-fi
-
-#Checking to see if the system is a Arch Linux
-if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Arch"* ]]; then
-    echo "You are using Arch Linux"
-    
-    #Updating
+}
+arch_do_updates()
+{
+        #Updating
     echo -e "\e[1;31m${bold}Updating System${normal}\e[0m"
     sudo yes | pacman -Syu && echo "System Updated"
     
+}
+arch_install_yay()
+{
+        
     #Installing Yay
-    read -p "Do you want to install Yay Pacman Helper? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Yay Pacman Helper? (Y/n) " ; then
         pacman -S --needed git base-devel
         git clone https://aur.archlinux.org/yay-bin.git
         cd yay-bin || exit
@@ -151,16 +181,84 @@ if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Arch"* ]]; then
     else
         echo "Moving On"
     fi
-        
+}
+arch_install_flatpak()
+{
     #Installing Flatpak
-    read -p "Do you want to install Flatpak? (Y/n) " confirmation
-    if [[ $confirmation =~ ^[Yy]$ ]] | [ -z "$confirmation" ]; then
+    if yesno "Do you want to install Flatpak? (Y/n) " ; then
         sudo yes | pacman -S flatpak && echo "Flatpak Installed"
     else
         echo "Moving On"
     fi
-else 
-    echo "You are not using Arch Linux checking others"
-fi
+}
 
-echo -e "\e[1;31m${bold}Finished!${normal}\e[0m"
+post_install_nobara()
+{
+    echo "You are using Nobara"
+    bashbar 0
+    
+    update
+    bashbar 10
+
+    install_flatpak
+    bashbar 20
+    
+    install_theming
+    bashbar 30
+    install_libre_office
+    bashbar 40
+    install_gaming
+    bashbar 50
+    install_photo_tools
+    bashbar 60
+    install_virtualisation
+    bashbar 70
+    download_configs
+    bashbar 80
+    copy_configs
+    bashbar 85
+    cleanup_precopied_files
+    bashbar 90
+    install_wallpaper
+    bashbar 95
+    apply_settings
+    bashbar 100
+}
+post_isntall_arch()
+{
+        echo "You are using Arch Linux"
+        bashbar 0
+        arch_do_updates
+        bashbar 30
+        arch_install_yay
+        bashbar 60
+        arch_install_flatpack
+        bashbar 100
+}
+
+main()
+{
+    
+
+    bold=$(tput bold)
+    normal=$(tput sgr0)
+    cd ~/ || exit
+
+    #Checking to see if the system is a Nobara Linux
+    if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Nobara"* ]]; then
+        post_install_nobara
+    else 
+        echo "You are not using Nobara checking others"
+    fi
+
+    #Checking to see if the system is a Arch Linux
+    if [[ $(grep PRETTY /etc/os-release | cut -c 13-) = *"Arch"* ]]; then
+        post_install_arch
+    else 
+        echo "You are not using Arch Linux checking others"
+    fi
+    
+    echo -e "\e[1;31m${bold}Finished!${normal}\e[0m"
+}
+
+main "$@"
