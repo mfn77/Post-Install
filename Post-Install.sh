74a677c08eec19479464713a84ef15a463616425
MFN <124898723+mfn77@users.noreply.github.com>
2023-04-13 12:35:20 +0300

    Added Full Arch Support

--- Post-Install.sh
+++ Post-Install.sh
@@ -242,6 +242,193 @@ arch_install_flatpak(){ #Installing Flatpak
     fi
 }
 
+arch_install_theming(){ #Installing Theming and Customizing Applications
+    if yes_reply "Do you want to install Customization Programs?" ; then
+        flatpak install com.mattjakeman.ExtensionManager -y && echo "Extension Manager Installed" || echo "Extension Manager Couldn't Installed"
+        flatpak install com.github.GradienceTeam.Gradience -y && echo "Gradience Installed" || echo "Gradience Couldn't Installed"
+        flatpak install org.gtk.Gtk3theme.adw-gtk3 -y && echo "Adwaita GTK 3 Theme for flatpaks Installed" || echo "Adwaita GTK 3 Theme for flatpaks Couldn't Installed"
+        flatpak install org.gtk.Gtk3theme.adw-gtk3-dark -y && echo "Adwaita GTK 3 Dark Theme for flatpaks Installed" || echo "Adwaita GTK 3 Dark Theme for flatpaks Couldn't Installed"
+        sudo yes | pacman -S alacarte && echo "Alacarte Menu Editor Installed" || echo "Alacarte Menu Editor Couldn't Installed"
+        sudo yes | pacman -S dconf-editor && echo "Dconf Editor Installed" || echo "Dconf Editor Couldn't Installed"
+        printf '\e[1;31mTheming And Customizing Applications Installed!\e[m\n'
+    else
+        echo "Moving on"
+    fi
+}
+
+arch_install_libre_office(){ #Installing LibreOffice
+    if yes_reply "Do you want to install LibreOffice?" ; then 
+        sudo yes | pacman -S libreoffice-fresh && echo "Libreoffice Installed" || echo "Libreoffice Couldn't Installed"
+        sudo yes | pacman -S libreoffice-fresh-tr && echo "Libreoffice TR Language Pack Installed" || echo "Libreoffice TR Language Couldn't Installed"
+        wget -qO- https://raw.githubusercontent.com/PapirusDevelopmentTeam/papirus-libreoffice-theme/master/install-papirus-root.sh | sh && echo "Libreoffice Papirus Theme Installed" || echo "Libreoffice Papirus Theme Couldn't Installed"
+        printf '\e[1;31mLibreOffice Installed\e[m\n'
+    else
+        echo "Moving on"
+    fi
+}
+
+arch_install_gaming(){ #Installing Gaming Applications
+    if yes_reply "Do you want to install Gaming Applications?" ; then
+        flatpak install io.github.Foldex.AdwSteamGtk -y && echo "Steam Adwaita Theme Installed" || echo "Steam Adwaita Theme Couldn't Installed"
+        flatpak install com.heroicgameslauncher.hgl -y && echo "Heroic Games Launcher Installed" || echo "Heroic Games Launcher Couldn't Installed"
+        flatpak install com.valvesoftware.Steam -y && echo "Steam Installed" || echo "Steam Couldn't Installed"
+        printf '\e[1;31mGaming Applications Installed\e[m\n'
+    else
+        echo "Moving on"
+    fi
+}
+
+arch_install_photo_tools(){ #Installing Photo Editing and Drawing Applications
+    if yes_reply "Do you want to install Photo Editing and Drawing Applications?" ; then
+        sudo yes | pacman -S gimp && echo "GIMP Installed" || echo "GIMP Couldn't Installed"
+        sudo yes | pacman -S inkscape && echo "Inkscape Installed" || echo "Inkscape Couldn't Installed"
+        yay -S blender --noconfirm && echo "Blender Installed" || echo "Blender Couldn't Installed"
+        printf '\e[1;31mPhoto Editing and Drawing Applications Installed\e[m\n'
+    else
+        echo "Moving on"
+    fi
+}
+
+arch_install_virtualization(){ #Installing Virtualization Applications
+    if yes_reply "Do you want to install Virtualization Applications?" ; then
+        sudo yes | pacman -S gnome-boxes && echo "Boxes Installed" || echo "Boxes Couldn't Installed"
+        yay -S virtualbox-bin --noconfirm && echo "VirtualBox Installed" || echo "VİrtualBox Couldn't Installed"
+        yay -S virtualbox-bin-guest-iso --noconfirm && echo "VirtualBox Guest Additions Installed" || echo "VİrtualBox Guest AdditionsCouldn't Installed"
+        yay -S waydroid --noconfirm && echo "Waydroid Installed" || echo "Waydroid Couldn't Installed"
+        yay -S waydroid-script-git --noconfirm && echo "Waydroid Translation Script Installed" || echo "Waydroid Translation Script Couldn't Installed"
+        yay -S waydroid-image-gapps --noconfirm && echo "Waydroid Gapps Image Installed" || echo "Waydroid Gapps Image Couldn't Installed"
+        yay -S anbox-modules-dkms-git --noconfirm && echo "Waydroid Kernel Modules Installed" || echo "Waydroid Kernel Modules Couldn't Installed"
+        printf '\e[1;31mVirtualization Applications Installed\e[m\n'
+    else
+        echo "Moving on"
+    fi
+}
+
+arch_download_configs(){ #Downloading and Exctracting Configs
+    echo "Configurations Syncing"
+    cd ~/ || exit
+    echo "Downloading Configuration Files"
+    sudo yes | pacman -S git
+    git clone https://github.com/mfn77/Post-Install.git
+    cd ~/Post-Install/icons || exit
+    tar xvf Bibata-Modern-Ice.tar.xz
+    rm Bibata-Modern-Ice.tar.xz
+    cd ~/Post-Install/themes || exit
+    tar xvf adw-gtk3.tar.xz
+    tar xvf adw-gtk3-dark.tar.xz
+    rm adw-gtk3.tar.xz
+    rm adw-gtk3-dark.tar.xz
+    printf '\e[1;31mDownloading Configuration Files Finished!\e[m\n'
+}
+
+arch_copy_configs(){ #Copying Configs
+    cd ~/Post-Install || exit
+    cp -Rv config/{gtk-2.0,gtk-3.0,gtk-4.0,dconf,menus} ~/.config/
+    cp -Rv {font,themes,icons} ~/.local/share
+    cp -Rv chrome ~/.mozilla/firefox/*.default-release/
+    printf '\e[1;31mCopying Configuration Files Finished!\e[m\n'
+}
+
+arch_cleanup_precopied_files(){ #Removing the already copied unnecessary files
+    cd ~/ || exit
+    rm -rf ~/Post-Install
+    printf '\e[1;31mConfiguration Files Are Synced!\e[m\n'
+}
+
+arch_install_extensions(){ #Installing Extensions
+    if yes_reply "Do you want to install Gnome Extensions?" ; then
+    echo "Installing Gnome Extensions"
+        sudo yes | pacman -S wget
+        git clone https://github.com/bjarosze/gnome-bluetooth-quick-connect
+        cd gnome-bluetooth-quick-connect
+        echo "Bluetooth Qucik Connect downloaded"
+        make install
+        echo "Bluetooth Quick Connect has been installed"
+        cd ~ || exit
+        sudo rm -r gnome-bluetooth-quick-connect
+        wget https://extensions.gnome.org/extension-data/io.github.mreditor.gnome-shell-extensions.scroll-panel.v10.shell-extension.zip
+        echo "Scroll Panel downloaded"
+        gnome-extensions install io.github.mreditor.gnome-shell-extensions.scroll-panel.v10.shell-extension.zip
+        echo "Scroll Panel has been installed"
+        sudo rm -r gnome-shell-extension-scroll-panel
+        wget https://extensions.gnome.org/extension-data/widgetsaylur.v24.shell-extension.zip
+        echo "Aylur's Widgets downloaded"
+        gnome-extensions install widgetsaylur.v24.shell-extension.zip
+        echo "Aylur's Widgets has been installed"
+        sudo rm widgetsaylur.v24.shell-extension.zip
+        wget https://extensions.gnome.org/extension-data/ddtermamezin.github.com.v43.shell-extension.zip
+        echo "ddterm downloaded"
+        gnome-extensions install ddtermamezin.github.com.v43.shell-extension.zip
+        echo "ddterm has been installed"
+        sudo rm ddtermamezin.github.com.v43.shell-extension.zip
+        wget https://extensions.gnome.org/extension-data/gnome-ui-tuneitstime.tech.v17.shell-extension.zip
+        echo "Gnome 4x UI Improvements downloaded"
+        gnome-extensions install gnome-ui-tuneitstime.tech.v17.shell-extension.zip
+        echo "Gnome 4x UI Improvements has been installed"
+        sudo rm gnome-ui-tuneitstime.tech.v17.shell-extension.zip
+        wget https://extensions.gnome.org/extension-data/blur-my-shellaunetx.v45.shell-extension.zip
+        echo "Blur My Shell downloaded"
+        gnome-extensions install blur-my-shellaunetx.v45.shell-extension.zip
+        echo "Blur My Shell has been installed"
+        sudo rm blur-my-shellaunetx.v45.shell-extension.zip
+        wget https://extensions.gnome.org/extension-data/MaximizeToEmptyWorkspace-extensionkaisersite.de.v13.shell-extension.zip
+        echo "Maximize To Empty Workspace downloaded"
+        gnome-extensions install MaximizeToEmptyWorkspace-extensionkaisersite.de.v13.shell-extension.zip
+        echo "Maximize To Empty Workspace has been installed"
+        sudo rm MaximizeToEmptyWorkspace-extensionkaisersite.de.v13.shell-extension.zip
+        printf '\e[1;31mExtensions Installed!\e[m\n'
+    else
+        echo "Moving On"
+    fi
+}
+
+arch_install_wallpaper(){ #Installing Wallpapers
+    if yes_reply "Do you want to install Wallpapers?" ; then
+    echo "Installing Wallpapers"
+        git clone https://github.com/saint-13/Linux_Dynamic_Wallpapers.git
+        cd Linux_Dynamic_Wallpapers || exit
+        echo "Files downloaded"
+        sudo cp -r ./Dynamic_Wallpapers/ /usr/share/backgrounds/
+        sudo cp ./xml/* /usr/share/gnome-background-properties/
+        echo "Wallpapers has been installed"
+        cd ~ || exit
+        echo "Deleting files used only for the installation process"
+        sudo rm -r Linux_Dynamic_Wallpapers
+        printf '\e[1;31mWallpapers Installed!\e[m\n'
+    else
+        echo "Moving On"
+    fi
+}
+
+arch_enable_extensions(){ #Enabling Installed Extensions
+    echo "Applying Some Settings"
+    gnome-extensions enable user-theme@gnome-shell-extensions.gcampax.github.com
+    gnome-extensions enable widgets@aylur
+    gnome-extensions enable io.github.mreditor.gnome-shell-extensions.scroll-panel
+    gnome-extensions enable ddterm@amezin.github.com
+    gnome-extensions enable blur-my-shell@aunetx
+    gnome-extensions enable bluetooth-quick-connect@bjarosze.gmail.com
+    gnome-extensions enable MaximizeToEmptyWorkspace-extension@kaisersite.de
+    gnome-extensions enable gnome-ui-tune@itstime.tech
+    gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+    gnome-extenisons enable gamemode@christian.kellner.me
+    gnome-extensions enable just-perfection-desktop@just-perfection
+    gnome-extenisons enable wireless-hid@chlumskyvaclav.gmail.com
+    printf '\e[1;31mExtensions Enabled!\e[m\n'
+}
+
+arch_apply_settings(){ #Applying Some Settings
+    echo "Applying Some Settings"
+    fc-cache -r
+    gsettings set org.gnome.desktop.interface text-scaling-factor 1.15
+    gsettings set org.gnome.desktop.interface gtk-theme adw-gtk3
+    gsettings set org.gnome.shell.extensions.user-theme name MFN
+    gsettings set org.gnome.desktop.interface cursor-theme Bibata-Modern-Ice
+    gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+    gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/Dynamic_Wallpapers/MaterialMountains/MaterialMountains-1.png
+    printf '\e[1;31mSettings Applied!\e[m\n'
+}
+
 post_install_nobara(){
     echo "You are using Nobara"
     update_system; bashbar 10
@@ -262,9 +449,21 @@ post_install_nobara(){
 
 post_isntall_arch(){
     echo "You are using Arch Linux"
-    arch_do_updates; bashbar 30
-    arch_install_yay; bashbar 60
-    arch_install_flatpak; bashbar 100
+    arch_do_updates; bashbar 10
+    arch_install_yay; bashbar 15
+    arch_install_flatpak; bashbar 20
+    arch_install_theming; bashbar 25
+    arch_install_libre_office; bashbar 30
+    arch_install_gaming; bashbar 40
+    arch_install_photo_tools; bashbar 50
+    arch_install_virtualization; bashbar 60
+    arch_download_configs; bashbar 65
+    arch_copy_configs; bashbar 70
+    arch_cleanup_precopied_files; bashbar 75
+    arch_install_extensions; bashbar 85
+    arch_install_wallpaper; bashbar 90
+    arch_enable_extensions; bashbar 95
+    arch_apply_settings; bashbar 100
 }
 
 main(){
